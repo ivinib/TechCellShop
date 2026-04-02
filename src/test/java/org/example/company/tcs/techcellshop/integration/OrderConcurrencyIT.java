84dfdba8aa982c4185bf7ddf5bf25170094e4826
ivinib <vi_ni_b@hotmail.com>
2026-04-02 14:40:29 -0300

    Initial implementation of Ci builds

--- src/test/java/org/example/company/tcs/techcellshop/integration/OrderConcurrencyIT.java
+++ src/test/java/org/example/company/tcs/techcellshop/integration/OrderConcurrencyIT.java
@@ -13,7 +13,7 @@ import org.springframework.http.*;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
